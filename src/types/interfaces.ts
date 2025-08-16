4509f1a706c8e46ea50af83c46331e538493a4c7
jagannadham112 <83686491+jagannadham112@users.noreply.github.com>
2025-08-16 09:19:59 +0100

    Show movie review excerpts; Refactor Details page.

--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -52,5 +52,10 @@ export type FilterOption = "title" | "genre";
 export interface MovieListPageTemplateProps extends BaseMovieListProps {
   title: string;
 }
+  export interface Review{
+    id: string;
+    content: string
+    author: string
+  }
 
 
